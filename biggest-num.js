6c7e313666d5d4fc2ee0d561f261f49d9be2d0a3
Muaaweyah Ibn Noufal K.P <muaaweyahkp@gmail.com>
2024-10-16 22:50:04 +0530

    biggest-number

---
+++ biggest-num.js
@@ -0,0 +1,13 @@
+let num1 = 25;
+let num2 = 150;
+let num3 = 45;
+
+if (num1>num2 && num1>num3){
+    console.log("num1 is larger")
+}
+else if(num2>num1 && num2>num3){
+    console.log("num2 is larger")
+}
+else{
+    console.log("num3 is larger")
+}
